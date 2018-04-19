08457108f3ea9ca3a4fa26053bbcae47b6dbc9e1
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-19 16:11:57 -0700

    Tested is_mel

--- party.py
+++ party.py
@@ -8,7 +8,21 @@ app.secret_key = "SECRETSECRETSECRET"
 
 
 def is_mel(name, email):
-    """Is this user Mel?"""
+    """Is this user Mel?
+
+    >>> is_mel('Mel Melitpolski', 'mel@ubermelon.com')
+    True
+
+    >>> is_mel('Judith Butler', 'judith@awesome.com')
+    False
+
+    >>> is_mel('Mel Melitpolski', 'bob@ubermelon.com')
+    True
+
+    >>> is_mel('Bob Dole', 'mel@ubermelon.com')
+    True
+
+    """
 
     return name == "Mel Melitpolski" or email == "mel@ubermelon.com"
 
